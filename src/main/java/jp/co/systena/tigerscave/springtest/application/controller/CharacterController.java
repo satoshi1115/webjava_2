d27ca004818fa4382627acbbd256aa50f0e91409
Systena <Systena@DESKTOP-IHALL7F>
2019-12-06 12:42:00 +0900

    WebJava課題２_20191206

--- src/main/java/jp/co/systena/tigerscave/springtest/application/controller/CharacterController.java
+++ src/main/java/jp/co/systena/tigerscave/springtest/application/controller/CharacterController.java
@@ -1,29 +1,32 @@
 package jp.co.systena.tigerscave.springtest.application.controller;
 
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import jp.co.systena.tigerscave.springtest.application.model.CharacterForm;
+import jp.co.systena.tigerscave.springtest.application.model.Work;
+import jp.co.systena.tigerscave.springtest.application.service.WorkService;
 
 @Controller
 public class CharacterController {
 
-  // @Autowired
-  // ListService listService;
+  @Autowired
+  WorkService workservice;
+
 
 
   // (valueの箇所はサーバーで起動したときのURLの場所。)
-  @RequestMapping(value = "/", method = RequestMethod.GET)
+  // @RequestMappingの記述を簡略化するために@GetMappingを使用
+  @GetMapping(value = "/")
   public ModelAndView list(ModelAndView mav) {
 
     // CharacterForm型のcharacterメソッドを使用。
     /*
      * テンプレートに値を渡す内容を設定 第一引数は「変数」、第二引数は実際に渡す「オブジェクト」
      */
-
-    CharacterForm character = new CharacterForm();
-    mav.addObject("character", character);
 
     // テンプレートファイル名を入力
     mav.setViewName("Character");
@@ -32,15 +35,33 @@ public class CharacterController {
 
   }
 
-  @RequestMapping(value = "/Command", method = RequestMethod.POST)
-  public ModelAndView information(ModelAndView mav) {
 
-    if (mav != null) {
+  /*
+   * CharacterForm型（CharacterFormクラス）の引数が入力された値を持っているため、 CharacterForm型を引数に持たせる。
+   */
+  // @RequestMappingの記述を簡略化するために@PostMappingを使用
+  @PostMapping(value = "/command")
+  public ModelAndView information(ModelAndView mav, CharacterForm characterform) {
+
+    if (characterform != null) {
+
+      /*
+       * CharacterForm型の新しいオブジェクトを作成する。 (オブジェクトを作成するとその型に含まれていた値は引き継がれない。
+       * オブジェクトを作成時に「new」をしているため新しい型として引き継がれる。)
+       */
       CharacterForm character = new CharacterForm();
+      Map<Integer, Work> itemListMap = workservice.getworklist();
+
+
+      // 変数characterオブジェクトを用いてに引数のcharacterformが持っていた値をsetさせる。
+      character.setSelectedRadio(characterform.getSelectedRadio());
+      character.setName(characterform.getName());
+
+
       mav.addObject("character", character);
 
       // Viewのテンプレート名を設定
-      mav.setViewName("ListView");
+      mav.setViewName("Command");
 
 
     }
--- src/main/java/jp/co/systena/tigerscave/springtest/application/model/CharacterForm.java
+++ src/main/java/jp/co/systena/tigerscave/springtest/application/model/CharacterForm.java
@@ -1,33 +1,25 @@
 package jp.co.systena.tigerscave.springtest.application.model;
 
+import java.util.LinkedHashMap;
+
 public class CharacterForm {
 
-
-  private String warrior;
-  private String magician;
+  private String selectedRadio;
   private String name;
 
-  public String getWarrior() {
-    return warrior;
+
+  public String getSelectedRadio() {
+    LinkedHashMap<Integer, String> selectMap = new LinkedHashMap<Integer, String>();
+
+    selectMap.put(1, "戦士");
+    selectMap.put(2, "魔法使い");
+
+
+    return selectedRadio;
   }
 
-  public void setWarrior(String warrior) {
-    this.warrior = warrior;
-  }
-
-  public String getMagician() {
-    return magician;
-  }
-
-  public void setMagician(String magician) {
-    this.magician = magician;
-  }
-
-
-  public String getName(String name) {
-    this.name = name;
-
-    return name;
+  public void setSelectedRadio(String selectedradio) {
+    this.selectedRadio = selectedradio;
   }
 
   public String getName() {
